660ce61fb9f780728cf2d4afeebb0048df1c1f03
odilijus-grinkas <odilijus.grinkas@stud.ktmc.lt>
2024-02-20 11:11:46 +0200

    changed schema again

--- db_scripts/script1finance.sql
+++ db_scripts/script1finance.sql
@@ -8,16 +8,22 @@ CREATE TABLE IF NOT EXISTS users (
     id INT(11) PRIMARY KEY AUTO_INCREMENT, email VARCHAR(30) UNIQUE, 
     password_hash VARCHAR(100)
 );
--- cashflow = income/expense, item_group = food/clothes etc.
-CREATE TABLE IF NOT EXISTS items (
-    id INT(11) PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), 
-    amount DECIMAL(10, 2), cashflow VARCHAR(10), 
-    date DATE NOT NULL, users_id INT(11), 
+CREATE TABLE IF NOT EXISTS transaction_group(
+    id INT(11) PRIMARY KEY AUTO_INCREMENT, 
+    name VARCHAR(20),
+    users_id INT(11), 
     FOREIGN KEY (users_id) REFERENCES users (id) ON DELETE CASCADE
 );
-CREATE TABLE IF NOT EXISTS transaction_groups(
-    id INT(11) PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20),
-    users_id INT(11), FOREIGN KEY (users_id) REFERENCES users (id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS items (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT, 
+    name VARCHAR(20), 
+    amount DECIMAL(10, 2), 
+    cashflow VARCHAR(10), 
+    date DATE NOT NULL, 
+    users_id INT(11), 
+    transaction_group_id INT(11),
+    FOREIGN KEY (users_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (transaction_group_id) REFERENCES transaction_group (id) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS tokens (
--- db_scripts/script2exampleData.sql
+++ db_scripts/script2exampleData.sql
@@ -2,17 +2,21 @@ use finance;
 INSERT INTO users (email, password_hash) VALUES
 ('user@email.com','$2b$10$Si9v2Illjwh34as/oj5lDuDRM3bE7aYQHaOCPje0dsvLSZXSc3Idm'),
 ('user2@email.com','$2b$10$PZNZyOSQGrBdK2Yy2RWii.cW4VYHi0t/y.Ea1G1H9qPWk1Hk7ojY2');
-INSERT INTO items (name, amount,cashflow, date,users_id) VALUES
-('salary',5000,'income', CURDATE(),1),
-('scratch tickets',5.20,'income', CURDATE(),1),
-('big prize',90.20,'income', CURDATE(),1),
-('groceries',120.30,'expense', CURDATE(),1),
-('peanuts',9001.35,'expense', CURDATE(),1),
-('meat',3301.44,'expense', CURDATE(),1),
-('rabbit',33.14,'expense', CURDATE(),1),
-('cat',99.14,'expense', CURDATE(),1),
-('pizza',9.00,'expense', CURDATE(),1),
-('ice-cream',10.00,'expense', CURDATE(),1),
-('gift cards',999.30,'income',CURDATE(),2),
-('food',1320.30,'expense', CURDATE(),2),
-('banana',555.30,'expense', CURDATE(),2);
+INSERT INTO transaction_group (name, users_id) VALUES
+('income group', 1),
+('expense group', 1),
+('another group', 1);
+INSERT INTO items (name, amount,cashflow, date, users_id, transaction_group_id) VALUES
+('salary',5000,'income', CURDATE(),1,1),
+('scratch tickets',5.20,'income', CURDATE(),1,1),
+('big prize',90.20,'income', CURDATE(),1,1),
+('groceries',120.30,'expense', CURDATE(),1,2),
+('peanuts',9001.35,'expense', CURDATE(),1,2),
+('meat',3301.44,'expense', CURDATE(),1,3),
+('rabbit',33.14,'expense', CURDATE(),1,3),
+('cat',99.14,'expense', CURDATE(),1,2),
+('pizza',9.00,'expense', CURDATE(),1,2),
+('ice-cream',10.00,'expense', CURDATE(),1,2);
+-- ('gift cards',999.30,'income',CURDATE(),2),
+-- ('food',1320.30,'expense', CURDATE(),2),
+-- ('banana',555.30,'expense', CURDATE(),2);
